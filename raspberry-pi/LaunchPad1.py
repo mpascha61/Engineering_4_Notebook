8c2f7fefea26cdbfa6bbff247ce4211e3cd19c6a
Malachi Paschal <mpascha61@charlottesvilleschools.org>
2022-09-12 14:06:53 -0400

    hgf

--- raspberry-pi/LaunchPad1.py
+++ raspberry-pi/LaunchPad1.py
@@ -1,6 +1,14 @@
+#type: ignore
 import time 
+import board
+import digitalio
+
+  led = digitalio.DigitalInOut(GP12.LED)
+  led.direction = digitalio.Direction.OUTPUT
+
 for counter in range(10, 0, -1):
   print(counter)
   time.sleep(1)
+
   
 print("Liftoff!")
