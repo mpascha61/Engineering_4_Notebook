4bf50f102f287e0d11f614b079fc62582e0f306d
mpascha61 <71345181+mpascha61@users.noreply.github.com>
2022-09-12 14:05:32 -0400

    Update LaunchPad3.py

--- raspberry-pi/LaunchPad3.py
+++ raspberry-pi/LaunchPad3.py
@@ -1,1 +1,26 @@
+#type: ignore
+import time 
+import digitalio 
+import board 
 
+led1 = digitalio.DigitalInOut(board.GP12)
+led2 = digitalio.DigitalInOut(board.GP19)
+led1.direction = digitalio.Direction.OUTPUT
+led2.direction = digitalio.Direction.OUTPUT
+
+for counter in range(10, -1, -1):
+  if counter is not 0:
+    print(counter)
+    led1.value = True
+    time.sleep(.5)
+    led1.value = False
+    time.sleep(.5)
+  else:
+    print("Liftoff!")
+    led2.value = True
+    time.sleep(.5)
+    led2.value = False 
+    time.sleep(.5)
+
+while True: 
+  pass
