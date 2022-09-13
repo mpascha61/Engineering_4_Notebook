b9f024f6006370984bc775ecc34c474ed5b0e283
mpascha61 <71345181+mpascha61@users.noreply.github.com>
2022-09-13 13:38:48 -0400

    Update LaunchPad3.py

--- raspberry-pi/LaunchPad3.py
+++ raspberry-pi/LaunchPad3.py
@@ -7,25 +7,25 @@ led1 = digitalio.DigitalInOut(board.GP12)
 led2 = digitalio.DigitalInOut(board.GP19)
 led1.direction = digitalio.Direction.OUTPUT
 led2.direction = digitalio.Direction.OUTPUT
-button = digitalio.DigialInOut(board.GP21)
+button = digitalio.DigitalInOut(board.GP21)
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.DOWN
 
 while True:
-    if button.value == True:
-  for counter in range(10, -1, -1):
-  if counter is not 0:
-    print(counter)
-    led1.value = True
-    time.sleep(.5)
-    led1.value = False
-    time.sleep(.5)
-  else:
-    print("Liftoff!")
-    led2.value = True
-    time.sleep(.5)
-    led2.value = False 
-    time.sleep(.5)
+  if button.value == True:
+    for counter in range(10, -1, -1):
+      if counter is not 0:
+       print(counter)
+      led1.value = True
+      time.sleep(.5)
+      led1.value = False
+      time.sleep(.5)
+    else:
+      print("Liftoff!")
+      led2.value = True
+      time.sleep(.5)
+      led2.value = False 
+      time.sleep(.5)
 
 while True: 
-  pass
+  passq
