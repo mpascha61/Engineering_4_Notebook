53f8cd0ea5a54365a9683a16197afc671ff3ca14
Malachi Paschal <mpascha61@charlottesvilleschools.org>
2022-09-16 13:59:30 -0400

    4444

--- raspberry-pi/LaunchPad4.py
+++ raspberry-pi/LaunchPad4.py
@@ -17,22 +17,22 @@ servo1 = servo.Servo(pwm_servo, min_pulse=500, max_pulse=2500)
 servo1.angle = 0
 
 while True:
-    if button.value == True:
-  for counter in range(10, -1, -1):
-  if counter is not 0:
-    print(counter)
-    led1.value = True
-    time.sleep(.5)
-    led1.value = False
-    time.sleep(.5)
+  if button.value == True:
+    for counter in range(10, -1, -1):
+      if counter is not 0:
+        print(counter)
+        led1.value = True
+        time.sleep(.5)
+        led1.value = False
+        time.sleep(.5)
 
-  else:
-    print("Liftoff!")
-    led2.value = True
-    time.sleep(.5)
-    led2.value = False 
-    time.sleep(.5)
-    servo1.angle = 180
+      else:
+        print("Liftoff!")
+        led2.value = True
+        time.sleep(.5)
+        led2.value = False 
+        time.sleep(.5)
+        servo1.angle = 180
 
 while True: 
   pass
