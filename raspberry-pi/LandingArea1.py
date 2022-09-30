f78c5e64e6ed48b4557759828d417e9cc997e3da
Malachi Paschal <mpascha61@charlottesvilleschools.org>
2022-09-30 14:07:22 -0400

    coordiantge

---
+++ raspberry-pi/LandingArea1.py
@@ -0,0 +1,16 @@
+#type: ignore
+
+while True:
+    print("Enter the first coordinate in format x,y:")
+    Coordinate1 = input()
+    print("Enter the second coordinate in format x,y:")
+    Coordinate2 = input()
+    print("Enter the third coordinate in format x,y:")
+    Coordinate3 = input()
+
+
+    x = Coordinate1.split(",")
+    y = Coordinate2.split(",")
+    z = Coordinate3.split(",")
+    
+    x = float(3)
