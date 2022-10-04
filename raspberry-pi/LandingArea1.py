e3d3ee3cb4118eca54cb867c380390ac7de323e1
Malachi Paschal <mpascha61@charlottesvilleschools.org>
2022-10-04 14:03:35 -0400

    gujgjy7jg

--- raspberry-pi/LandingArea1.py
+++ raspberry-pi/LandingArea1.py
@@ -1,5 +1,9 @@
 #type: ignore
 
+def area_calc(x1, x2, x3, y1, y2, y3): #Find the coordinates
+    area = abs((1/2) * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)))
+
+    return area
 while True:
     print("Enter the first coordinate in format x,y:")
     Coordinate1 = input()
@@ -7,21 +11,23 @@ while True:
     Coordinate2 = input()
     print("Enter the third coordinate in format x,y:")
     Coordinate3 = input()
-
-    Coordinate1 = Coordinate1.split(",")
-    print(Coordinate1)
+    try:
+        Coordinate1 = Coordinate1.split(",")
+        Coordinate2 = Coordinate2.split(",")
+        Coordinate3 = Coordinate3.split(",")
 
     #print(Coordinate1) <-- will print the (x,y)
     #print(Coordinate1[0]) <-- will print the first number (x), if i put [1] it will print the second number (y)
 
-    Coordinate2 = Coordinate2.split(",")
-    print(Coordinate2)
-    Coordinate3 = Coordinate3.split(",")
-    print(Coordinate3)
-    x = float(3)
-    try:
-        print(x,y)
+        x1 = float(Coordinate1[0])
+        x2 = float(Coordinate2[0])
+        x3 = float(Coordinate3[0])
+
+        y1 = float(Coordinate1[1])
+        y2 = float(Coordinate2[1])
+        y3 = float(Coordinate3[1])
+        area = area_calc(x1, x2, x3, y1, y2, y3)
+        ans = area
+        print(f"The area of the triangle with the vertices {Coordinate1}, {Coordinate2}, {Coordinate3} is {ans} kilometers squared")
     except:
         print("An exception occurred")
-
-  
