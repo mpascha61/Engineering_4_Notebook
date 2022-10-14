ecd916088757777c9f4d482454c86d0aeb052d57
Malachi Paschal <mpascha61@charlottesvilleschools.org>
2022-10-14 14:08:59 -0400

    kgughkj

---
+++ raspberry-pi/MorseCode1.py
@@ -0,0 +1,24 @@
+#type: ignore
+
+MORSE_CODE = { 'A':'.-', 'B':'-...',
+    'C':'-.-.', 'D':'-..', 'E':'.',
+    'F':'..-.', 'G':'--.', 'H':'....',
+    'I':'..', 'J':'.---', 'K':'-.-',
+    'L':'.-..', 'M':'--', 'N':'-.',
+    'O':'---', 'P':'.--.', 'Q':'--.-',
+    'R':'.-.', 'S':'...', 'T':'-',
+    'U':'..-', 'V':'...-', 'W':'.--',
+    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+    '1':'.----', '2':'..---', '3':'...--',
+    '4':'....-', '5':'.....', '6':'-....',
+    '7':'--...', '8':'---..', '9':'----.',
+    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+    '?':'..--..', '/':'-..-.', '-':'-....-',
+    '(':'-.--.', ')':'-.--.-'}
+while True:
+    print("Enter message for morse code, or enter -q to quit")
+    message = input()
+    for letter in message: 
+    # message is just my input, for letter in message means it will print each individual letter, but I want to convert each letter to morse code 
+        print(letter)
+    # use MORSE_CODE[letter] here to translate from input into morse code
